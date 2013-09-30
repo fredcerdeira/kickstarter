f5b1d82e20cf171041142a6cb2b40628367756a7
fredcerdeira <frederico.cerdeira@gmail.com>
2013-09-30 12:20:10 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>kickstarter PHP!</title>
+</head>
+<body>
+
+	<?php
+		$firstName = "Frederico";
+		$lastName  = "Cerdeira";
+		$fullname  = $firstName . " - " . $lastName;
+		echo $fullname; 
+	?>
+
+
+	
+</body>
+</html>
